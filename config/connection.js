d9493411c76611619eefb3f94a29e1688f9c4b47
Rod0352 <92071426+Rod0352@users.noreply.github.com>
2022-02-05 20:40:28 -0500

    exclude password from endpoint, restful api made for user end point

--- config/connection.js
+++ config/connection.js
@@ -1,13 +1,13 @@
 // import sequalize sequalize constructor from library
-const Sequalize = require('sequalize');
+const Sequelize = require('sequelize');
 
 require('dotenv').config();
 // All we're doing here is importing the base Sequelize class and using it to create a new connection to the database. The new Sequelize() function accepts the database name, MySQL username, and MySQL password (respectively) as parameters, then we also pass configuration settings. Once we're done, we simply export the connection.
 // create connection to our database, passing in mysql information
-const sequalize = new Sequalize('just_tech_news_db', 'username', 'password', {
+const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PW, {
     host: 'localhost',
     dialect: 'mysql',
     port: 3306
 });
 
-module.exports = sequalize;
+module.exports = sequelize;
